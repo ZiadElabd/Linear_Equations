13c961ab8e2972b746b9df7204f7e10a723dfd14
omarrehan0020 <59891874+omarrehan0020@users.noreply.github.com>
2020-12-17 13:38:41 +0200

    gauss seidel done

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ arr[0][1]=6;
 arr[0][2]=2;
 arr[0][3]=-2;
 arr[1][0]=2;
-arr[1][1]=0;
+arr[1][1]=6;
 arr[1][2]=5;
-arr[1][3]=-2;   
+arr[1][3]=2;   
 arr[2][0]=-4;
 arr[2][1]=-3;
 arr[2][2]=-5;
@@ -134,5 +134,37 @@ function gauss_gordan(equationArray,vec){
 
 }
 
-console.log(gauss_elimination_with_pivoting(arr,vec));
+//console.log(gauss_elimination_with_pivoting(arr,vec));
+
+
+function gauss_seidel (equation_factors, equations_value, max_degree, number_of_iters){
+
+    var initial_x=[],
+        results=[];
+    for(var i=0 ;i<max_degree ;i++) initial_x[i]=1; 
+    while(number_of_iters > 0){
+        for (var first_iterator = 0; first_iterator < max_degree; first_iterator++)
+        {
+            results[first_iterator] = (equations_value[first_iterator] / equation_factors[first_iterator][first_iterator]);
+           // console.log( results[first_iterator])
+            for (var second_iterator = 0; second_iterator < max_degree; second_iterator++)
+            {
+                if (second_iterator == first_iterator) // not for diagonals
+                    continue;
+                // the real deal happens below
+                results[first_iterator] = results[first_iterator] - ((equation_factors[first_iterator][second_iterator] / equation_factors[first_iterator][first_iterator]) * initial_x[second_iterator]);
+                //console.log( results[first_iterator])
+                
+            }
+            initial_x[first_iterator] = results[first_iterator];
+           console.log(initial_x);
+        }
+         number_of_iters--;
+    }
+
+    
+}
+
+//gauss_seidel(arr,vec,4,1);
+
 
